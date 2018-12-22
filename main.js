104feefaa0438d1a0e1857b5d9e882b241a8bf7d
Tech-Shujan <44917041+Tech-Shujan@users.noreply.github.com>
2018-12-22 10:36:29 +0530

    Add files via upload
    This is the login page........

---
+++ main.js
@@ -0,0 +1,7 @@
+function termsCon(){
+  document.getElementById("terms-con").style.display = "block";
+}
+function accepted(){
+  document.getElementById("submit").style.background = "#0084f6";
+  document.getElementById("terms-con").style.display = "none";
+}
